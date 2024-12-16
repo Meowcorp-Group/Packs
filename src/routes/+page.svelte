0088a0e3fec8e0fe34afa499bec8752147da6b9a
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-12-16 12:36:35 +0700

    language stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+	import * as m from '$lib/paraglide/messages.js'
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>{m.hello_world()}</p>
